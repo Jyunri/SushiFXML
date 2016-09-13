ca70b888115701082a0f0d8201f5fe96e5c3901a
Jonathan Suenaga <jonathansuenaga@Mac-Air-DMA10.local>
2016-09-13 16:40:44 -0300

    csv parse

--- src/sushitinafxml/FXMLDocumentController.java
+++ src/sushitinafxml/FXMLDocumentController.java
@@ -1,10 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package sushitinafxml;
 
+import com.opencsv.CSVReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -32,7 +36,7 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Button btConfirmar, btAdicionarItem;
-      
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -47,24 +51,45 @@ public class FXMLDocumentController implements Initializable {
         tfNome.clear();
         tfEndereco.clear();
     }
-            
+    
+    @FXML
+    private int buscarCliente(ActionEvent event) throws FileNotFoundException, IOException{
+        System.out.println("Buscando Clientes");
+        CSVReader reader = new CSVReader(new FileReader("ClienteCSV.csv"));
+        String telefone = tfTelefone.getText();
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            // nextLine[] is an array of values from the line
+            if(nextLine[1].equals(telefone))
+            {
+                System.out.println("Encontrou, Nome: "+nextLine[2]);
+                tfNome.setText(nextLine[2]);
+                tfCodigoCliente.setText(nextLine[0]);
+                tfEndereco.setText(nextLine[3]);
+                return 1;
+            }
+        }
+        return 0;
+    }
+    
+    
     @FXML
     private void adicionarItem(ActionEvent event) throws Exception{
         System.out.println("Adicionando itens");
         try {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("AddItem.fxml"));
-                Parent root1 = (Parent) fxmlLoader.load();
-                Stage stage = new Stage();
-                stage.setScene(new Scene(root1));  
-                stage.show();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/AddItem.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.show();
         } catch(Exception e) {
-           e.printStackTrace();
-          }
+            e.printStackTrace();
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
     
 }
